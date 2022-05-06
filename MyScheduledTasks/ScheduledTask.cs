e946c16c6de893260eaf424988147d8bf25d2ff0
Tim Kennedy <KennedyTim@outlook.com>
2022-05-05 21:02:33 -0500

    Add additional task details

--- MyScheduledTasks/ScheduledTask.cs
+++ MyScheduledTasks/ScheduledTask.cs
@@ -18,7 +18,7 @@ public class ScheduledTask : INotifyPropertyChanged
             {
                 TaskName = task.Name,
                 TaskStatus = task.State.ToString(),
-                TaskResult = task.LastTaskResult,
+                TaskResult = (uint?)task.LastTaskResult,
                 LastRun = task.LastRunTime,
                 NextRun = task.NextRunTime,
                 TaskPath = task.Path,
@@ -29,7 +29,12 @@ public class ScheduledTask : INotifyPropertyChanged
                 TaskRunLevel = task.Definition.Principal.RunLevel.ToString(),
                 TaskDescription = task.Definition.RegistrationInfo.Description,
                 TaskAuthor = task.Definition.RegistrationInfo.Author,
-                TaskTriggers = task.Definition.Triggers.ToString()
+                TaskTriggers = task.Definition.Triggers.ToString(),
+                Enabled = task.Definition.Settings.Enabled,
+                Priority = task.Definition.Settings.Priority.ToString(),
+                AllowDemandStart = task.Definition.Settings.AllowDemandStart,
+                TimeLimit = task.Definition.Settings.ExecutionTimeLimit.ToString(),
+                WakeToRun = task.Definition.Settings.WakeToRun
             };
             schedTask.TaskNote = item != null ? item.TaskNote : string.Empty;
             schedTask.IsChecked = item != null ? item.Alert : false;
@@ -41,7 +46,7 @@ public class ScheduledTask : INotifyPropertyChanged
 
     #region Private backing fields
     private bool isChecked;
-    private int taskResult;
+    private uint? taskResult;
     private string taskName;
     private string taskStatus;
     private string taskRunLevel;
@@ -49,9 +54,20 @@ public class ScheduledTask : INotifyPropertyChanged
     private DateTime? lastRun;
     private DateTime? nextRun;
     private string taskNote;
-    #endregion Private backing fields
 
+    #endregion Private backing fields
     #region Properties
+
+    public string TimeLimit { get; set; }
+
+    public bool AllowDemandStart { get; set; }
+
+    public bool WakeToRun { get; set; }
+
+    public bool Enabled { get; set; }
+
+    public string Priority { get; set; }
+
     public bool IsChecked
     {
         get => isChecked;
@@ -98,14 +114,14 @@ public class ScheduledTask : INotifyPropertyChanged
         }
     }
 
-    public int? TaskResult
+    public uint? TaskResult
     {
         get => taskResult;
         set
         {
             if (value != null)
             {
-                taskResult = (int)value;
+                taskResult = value;
                 OnPropertyChanged();
             }
         }
@@ -115,17 +131,30 @@ public class ScheduledTask : INotifyPropertyChanged
     {
         get
         {
-            if (TaskResult == null)
+            switch (TaskResult)
             {
-                return string.Empty;
-            }
-            else if (TaskResult == 0)
-            {
-                return "OK";
-            }
-            else
-            {
-                return "NZ";
+                case null:
+                    return string.Empty;
+                case 0:
+                    return "OK";
+                case 0x41300:
+                    return "RDY"; //Task is ready to run at its next scheduled time
+                case 0x41301:
+                    return "RUN"; //The task is currently running
+                case 0x41302:
+                    return "DIS"; //The task has been disabled
+                case 0x41303:
+                    return "NYR"; //The task has not yet run
+                case 0x41304:
+                    return "NMR"; //There are no more runs scheduled for this task
+                case 0x41306:
+                    return "TRM"; //The last run of the task was terminated by the user
+                case 0x8004131F:
+                    return "AR"; //An instance of this task is already running
+                case 0x80070002:
+                    return "FNF"; //File not found
+                default:
+                    return "NZ";
             }
         }
     }
