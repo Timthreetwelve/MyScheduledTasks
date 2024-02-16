35004ab1fa3c2e9e7f10cf897f8c5ad96b22dfda
Tim Kennedy <timthreetwelve@outlook.com>
2024-02-16 07:33:53 -0600

    Added refresh icon

---
+++ MyScheduledTasks/Converters/RefreshVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+namespace MyScheduledTasks.Converters;
+
+/// <summary>
+/// Show refresh button only on main page.
+/// </summary>
+internal class RefreshVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is MainViewModel ? Visibility.Visible : (object)Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+}
