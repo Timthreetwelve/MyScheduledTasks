6de9217d9220f073c6f1812e064ee98d488ce39f
Tim Kennedy <timthreetwelve@outlook.com>
2024-12-14 20:08:54 -0600

    Refactored property.

--- MyScheduledTasks/Converters/EnumBindingSourceExtension.cs
+++ MyScheduledTasks/Converters/EnumBindingSourceExtension.cs
@@ -14,24 +14,18 @@ internal sealed class EnumBindingSourceExtension : MarkupExtension
     private Type _enumType;
     public Type EnumType
     {
-        get { return _enumType; }
         set
         {
-            if (value != _enumType)
+            if (value == _enumType) return;
+            if (value != null)
             {
-                if (value != null)
-                {
-                    Type enumType = Nullable.GetUnderlyingType(value) ?? value;
-                    if (!enumType.IsEnum)
-                        throw new ArgumentException("Type must be for an Enum.");
-                }
-
-                _enumType = value;
+                Type enumType = Nullable.GetUnderlyingType(value) ?? value;
+                if (!enumType.IsEnum)
+                    throw new ArgumentException("Type must be for an Enum.");
             }
+            _enumType = value;
         }
     }
-
-    public EnumBindingSourceExtension() { }
 
     public EnumBindingSourceExtension(Type enumType)
     {
