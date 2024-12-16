30c3e4bc5e9c151144300b7e39f1f2022b3460bb
Tim Kennedy <timthreetwelve@outlook.com>
2024-12-15 19:33:54 -0600

    Removed unneeded property setters.

--- MyScheduledTasks/Models/AllTasks.cs
+++ MyScheduledTasks/Models/AllTasks.cs
@@ -4,8 +4,8 @@ namespace MyScheduledTasks.Models;
 
 internal sealed partial class AllTasks : ObservableObject
 {
-    public static ObservableCollection<AllTasks> All_TasksCollection { get; set; } = [];
-    public static ObservableCollection<AllTasks> Non_MS_TasksCollection { get; set; } = [];
+    public static ObservableCollection<AllTasks> All_TasksCollection { get; } = [];
+    public static ObservableCollection<AllTasks> Non_MS_TasksCollection { get; } = [];
 
     [ObservableProperty]
     private string? _taskPath;
